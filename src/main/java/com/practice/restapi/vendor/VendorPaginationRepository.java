363837e50fd7f64313741ed917cd8f9118141f5b
roopmitra86 <roopmitra86@gmail.com>
2026-03-07 16:04:41 +0530

    pagination

--- src/main/java/com/practice/restapi/vendor/VendorPaginationRepository.java
+++ src/main/java/com/practice/restapi/vendor/VendorPaginationRepository.java
@@ -1,6 +1,7 @@
 package com.practice.restapi.vendor;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
-public interface VendorPaginationRepository extends ListPagingAndSortingRepository<Vendor, Long> {
+public interface VendorPaginationRepository extends ListPagingAndSortingRepository<Vendor, Long>, CrudRepository<Vendor, Long> {
 }
