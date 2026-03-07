3087a8e6390a6a4d29a4587468bdbd16fc43709a
roopmitra86 <roopmitra86@gmail.com>
2026-03-08 01:32:41 +0530

    stream

---
+++ src/main/java/com/practice/restapi/employee/Employee.java
@@ -0,0 +1,53 @@
+package com.practice.restapi.employee;
+
+import jakarta.persistence.Entity;
+
+
+public class Employee {
+    String name;
+    int age;
+    String department;
+    double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int age, String department, double salary) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
--- src/main/java/com/practice/restapi/vendor/VendorService.java
+++ src/main/java/com/practice/restapi/vendor/VendorService.java
@@ -1,11 +1,11 @@
 package com.practice.restapi.vendor;
 
+import com.practice.restapi.employee.Employee;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class VendorService {
@@ -35,6 +35,34 @@ public class VendorService {
     }
 
     public List<Vendor> getAllVendors(int pageNumber, int pageSize){
-        return vendorRepository.findAll(PageRequest.of(pageNumber, pageSize)).getContent();
+        List<Vendor> vendors = vendorRepository.findAll(PageRequest.of(pageNumber, pageSize)).getContent();
+
+        List<Employee> employees = Arrays.asList(
+                new Employee("Employee1", 24, "Dept1", 12000),
+                new Employee("Employee2", 25, "Dept2", 15000),
+                new Employee("Employee3", 29, "Dept1", 16000),
+                new Employee("Employee4", 28, "Dept1", 17000),
+                new Employee("Employee5", 21, "Dept2", 19000),
+                new Employee("Employee6", 26, "Dept2", 14000)
+                );
+
+        Map<String, Optional<Employee>> groupedEmployees = employees.stream()
+                                                    .collect(Collectors.groupingBy(Employee::getDepartment,
+                                                                                    Collectors.collectingAndThen(
+                                                                                            Collectors.toList(),
+                                                                                            newList -> newList.stream()
+                                                                                                .sorted(Comparator.comparingDouble(Employee::getSalary).reversed())
+                                                                                                    .skip(1)
+                                                                                                    .findFirst()
+                                                                                    )));
+
+        groupedEmployees.forEach((dept, employee) -> {
+            System.out.print("Dept: "+ dept + "|");
+            employee.ifPresentOrElse(
+                    emp -> System.out.println("2nd highest: " + emp.getName() + "($" + emp.getSalary() + ")"),
+                    () -> System.out.println("2nd highsest not found")
+            );
+        });        ;
+        return vendors;
     }
 }
